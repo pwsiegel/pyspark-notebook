79b33640f73abdfd5685040b899b085fb6975922
Paul Siegel <pwsiegel@gmail.com>
2019-04-14 13:45:05 -0400

    remove memory args from run-spark-notebook script

--- bin/run-spark-notebook.sh
+++ bin/run-spark-notebook.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 export PORT=8888
-export EXECUTOR_MEMORY=220g
-export DRIVER_MEMORY=30g
 
 export PYSPARK_DRIVER_PYTHON=jupyter
 export PYSPARK_DRIVER_PYTHON_OPTS="lab --no-browser --port=$PORT"
 
 pyspark --packages "org.apache.hadoop:hadoop-aws:3.1.0" \
         --master spark://"$(hostname -I | tr -d ' ')":7077 \
-        --executor-memory "$EXECUTOR_MEMORY" \
-        --driver-memory "$DRIVER_MEMORY" \
