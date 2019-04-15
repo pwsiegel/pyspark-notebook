f6ad85c9dcc901f22a2e3e2e8c6cbeac578be6b3
Paul Siegel <pwsiegel@gmail.com>
2019-04-15 10:52:49 -0400

    increase s3a connection pool in pyspark conf

--- run-spark-notebook.sh
+++ run-spark-notebook.sh
@@ -4,13 +4,11 @@ export PORT=8888
 export DRIVER_MEMORY=30G
 export EXECUTOR_MEMORY=220G
 
-export PYSPARK_SUBMIT_ARGS="--packages org.apache.hadoop:hadoop-aws:3.1.0 pyspark-shell"
 export PYSPARK_DRIVER_PYTHON=jupyter
 export PYSPARK_DRIVER_PYTHON_OPTS="lab --no-browser --port=$PORT"
 
-echo Spark master address: spark://"$(hostname -I | tr -d ' ')":7077
-
 pyspark --packages "org.apache.hadoop:hadoop-aws:3.1.0" \
         --master spark://"$(hostname -I | tr -d ' ')":7077 \
-		--executor-memory "$EXECUTOR_MEMORY" \
+        --executor-memory "$EXECUTOR_MEMORY" \
         --driver-memory "$DRIVER_MEMORY" \
+        --conf spark.hadoop.fs.s3a.connection.maximum=1000 \
