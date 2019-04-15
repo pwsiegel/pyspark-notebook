051c08175861476e94e3eae396aa8b717acfd261
Paul Siegel <pwsiegel@gmail.com>
2019-04-15 09:05:25 -0400

    print spark master address in run-spark script

--- run-spark-notebook.sh
+++ run-spark-notebook.sh
@@ -8,6 +8,8 @@ export PYSPARK_SUBMIT_ARGS="--packages org.apache.hadoop:hadoop-aws:3.1.0 pyspar
 export PYSPARK_DRIVER_PYTHON=jupyter
 export PYSPARK_DRIVER_PYTHON_OPTS="lab --no-browser --port=$PORT"
 
+echo Spark master address: spark://"$(hostname -I | tr -d ' ')":7077
+
 pyspark --packages "org.apache.hadoop:hadoop-aws:3.1.0" \
         --master spark://"$(hostname -I | tr -d ' ')":7077 \
 		--executor-memory "$EXECUTOR_MEMORY" \
