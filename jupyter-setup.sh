f5fdbc9591eb70bdf6c6ce8c598ce14e5955d4f0
Paul Siegel <pwsiegel@gmail.com>
2019-04-15 15:19:30 -0400

    add commands to install jupyterlab-vim plugin

--- jupyter-setup.sh
+++ jupyter-setup.sh
@@ -9,3 +9,6 @@ source "$HOME"/.bashrc
 pyenv install 3.7.2
 pyenv global 3.7.2
 pip install -r requirements.txt
+curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
+sudo yum install -y nodejs
+jupyter labextension install jupyterlab_vim
