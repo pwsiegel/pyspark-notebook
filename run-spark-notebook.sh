d228dc87183468937c6b2759f7128367f6c9dd27
Paul Siegel <pwsiegel@gmail.com>
2019-04-16 11:26:11 -0400

    add trailing backslash in run-spark-notebook

--- run-spark-notebook.sh
+++ run-spark-notebook.sh
@@ -12,6 +12,6 @@ pyspark --packages "org.apache.hadoop:hadoop-aws:3.1.0" \
         --master spark://"$(hostname -I | tr -d ' ')":7077 \
         --executor-memory "$EXECUTOR_MEMORY" \
         --driver-memory "$DRIVER_MEMORY" \
-        --conf spark.driver.maxResultSize="$MAX_RESULTS_SIZE"
+        --conf spark.driver.maxResultSize="$MAX_RESULTS_SIZE" \
         --conf spark.hadoop.fs.s3a.connection.maximum=1000 \
         --conf spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version=2
