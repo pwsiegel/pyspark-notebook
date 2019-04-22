ac0bf8a35302d63d707eacdb848dcfeb8c3e7375
Paul Siegel <pwsiegel@gmail.com>
2019-04-22 15:35:43 -0400

    change = to ==

--- sparknb-flintrock.sh
+++ sparknb-flintrock.sh
@@ -6,7 +6,7 @@ port="$2"
 master=$(flintrock describe "$cluster" | grep master | awk '{ print $NF }' | tr -d "\n")
 
 if [ -z "$3" ]; then
-    if [ "$3" = "agent" ]; then
+    if [ "$3" == "agent" ]; then
         ssh -L -T "$port":localhost:"$port" ec2-user@"$master"
     else
         exit 1
