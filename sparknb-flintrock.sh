fdcf6f83f76fb87810a8fc36a0942916aa48bd5b
Paul Siegel <pwsiegel@gmail.com>
2019-04-22 16:09:10 -0400

    use parameter default values for --agent flag

--- sparknb-flintrock.sh
+++ sparknb-flintrock.sh
@@ -3,14 +3,16 @@ set -eou pipefail
 
 cluster="$1"
 port="$2"
+agent=${3:-}
 master=$(flintrock describe "$cluster" | grep master | awk '{ print $NF }' | tr -d "\n")
 
-if [ -z "$3" ]; then
-    if [ "$3" == "agent" ]; then
-        ssh -L -T "$port":localhost:"$port" ec2-user@"$master"
+if [[ -z "$agent" ]]; then
+    ssh -L "$port":localhost:"$port" ec2-user@"$master"
+else
+    if [ "$agent" == "--agent" ]; then
+        ssh -L -A "$port":localhost:"$port" ec2-user@"$master"
     else
+        echo "Illegal argument $agent; the third argument can only be the flag --agent to enable agent forwarding"
         exit 1
     fi
-else
-    ssh -L "$port":localhost:"$port" ec2-user@"$master"
 fi
