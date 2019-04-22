d54f5b0d4bc17d57a3667bae0ef686ceb26fb7b5
Paul Siegel <pwsiegel@gmail.com>
2019-04-22 14:23:48 -0400

    fill out flintrock script

--- sparknb-flintrock.sh
+++ sparknb-flintrock.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 set -eou pipefail
+
+cluster="$1"
+port="$2"
+master=$(flintrock describe "$cluster" | grep master | awk '{ print $NF }' | tr -d "\n")
+
+if [ -z "$3" ]; then
+    if [ "$3" = "agent" ]; then
+        ssh -L -T "$port":localhost:"$port" ec2-user@"$master"
+    else
+        exit 1
+    fi
+else
+    ssh -L "$port":localhost:"$port" ec2-user@"$master"
+fi
