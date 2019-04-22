1dcef638aed08e022cb58bfd13c1463395b0a49a
Paul Siegel <pwsiegel@gmail.com>
2019-04-22 16:35:43 -0400

    eliminate --agent flag as -A flag isn't needed for agent forwarding

--- sparknb-flintrock.sh
+++ sparknb-flintrock.sh
@@ -3,16 +3,6 @@ set -eou pipefail
 
 cluster="$1"
 port="$2"
-agent=${3:-}
 master=$(flintrock describe "$cluster" | grep master | awk '{ print $NF }' | tr -d "\n")
 
-if [[ -z "$agent" ]]; then
-    ssh -L "$port":localhost:"$port" ec2-user@"$master"
-else
-    if [ "$agent" == "--agent" ]; then
-        ssh -L -A "$port":localhost:"$port" ec2-user@"$master"
-    else
-        echo "Illegal argument $agent; the third argument can only be the flag --agent to enable agent forwarding"
-        exit 1
-    fi
-fi
+ssh -L "$port":localhost:"$port" ec2-user@"$master"
