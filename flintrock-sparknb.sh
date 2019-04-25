c9ba88ece9b438fa44ec9fb3a976d507995d8e06
Paul Siegel <pwsiegel@gmail.com>
2019-04-25 12:43:18 -0400

    error handling for arg reading

--- flintrock-sparknb.sh
+++ flintrock-sparknb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eou pipefail
 
-cluster="$1"
-port="$2"
+cluster=${1:?Usage: ./setup-sparknb.sh <flintrock-cluster-name> <port-number>}
+port=${2:?Usage: ./setup-sparknb.sh <flintrock-cluster-name> <port-number>}
 master=$(flintrock describe "$cluster" | grep master | awk '{ print $NF }' | tr -d "\n")
 
 ssh -L "$port":localhost:"$port" ec2-user@"$master"
