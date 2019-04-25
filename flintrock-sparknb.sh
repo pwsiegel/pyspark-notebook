e7a99c872a107e4668dc773e372ecadb4843303b
Paul Siegel <pwsiegel@gmail.com>
2019-04-25 12:45:32 -0400

    replace grep and awk with --master-hostname-only flag

--- flintrock-sparknb.sh
+++ flintrock-sparknb.sh
@@ -3,6 +3,6 @@ set -eou pipefail
 
 cluster=${1:?Usage: ./setup-sparknb.sh <flintrock-cluster-name> <port-number>}
 port=${2:?Usage: ./setup-sparknb.sh <flintrock-cluster-name> <port-number>}
-master=$(flintrock describe "$cluster" | grep master | awk '{ print $NF }' | tr -d "\n")
+master=$(flintrock describe "$cluster" --master-hostname-only)
 
 ssh -L "$port":localhost:"$port" ec2-user@"$master"
