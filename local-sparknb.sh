b2ac1abda66e5cd7cce7bdb6715983239517d89e
Paul Siegel <pwsiegel@gmail.com>
2019-04-30 13:07:30 -0400

    add local-sparknb script

---
+++ local-sparknb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eou pipefail
+
+port=${1:?Usage: ./run-sparknb.sh <port-number>}
+
+DRIVER_MEMORY=4G
+EXECUTOR_MEMORY=8G
+MAX_RESULTS_SIZE=4G
+
+export PYSPARK_DRIVER_PYTHON=jupyter
+export PYSPARK_DRIVER_PYTHON_OPTS="lab --no-browser --notebook-dir=$HOME --port=$port"
+
+pyspark --packages "org.apache.hadoop:hadoop-aws:3.1.0" \
+        --master local[*] \
+        --executor-memory "$EXECUTOR_MEMORY" \
+        --driver-memory "$DRIVER_MEMORY" \
+        --conf spark.driver.maxResultSize="$MAX_RESULTS_SIZE" \
+        --conf spark.hadoop.fs.s3a.connection.maximum=1000 \
+        --conf spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version=2
