378e88951c599538b24ed9e132f8c3d4f6a43746
Paul Siegel <pwsiegel@gmail.com>
2019-11-25 15:02:19 -0500

    use FLINTROCK_CONFIG_PATH environmental variable in flintrock-nb script

--- flintrock-sparknb.sh
+++ flintrock-sparknb.sh
@@ -3,6 +3,6 @@ set -eou pipefail
 
 cluster=${1:?Usage: ./setup-sparknb.sh <flintrock-cluster-name> <port-number>}
 port=${2:?Usage: ./setup-sparknb.sh <flintrock-cluster-name> <port-number>}
-master=$(flintrock describe "$cluster" --master-hostname-only)
+master=$(flintrock --config "$FLINTROCK_CONFIG_PATH" describe "$cluster" --master-hostname-only)
 
 ssh -L "$port":localhost:"$port" ec2-user@"$master"
